e358c45959b14381ce922803fd649d8bdc23ee9d
ArcticPass <riley_odlaw@hotmail.com>
2020-07-21 21:30:05 -0600

    Added crab NPC

---
+++ Assets/Scripts/Combat/BodyDisposal.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+using RPG.Stats;
+
+namespace RPG.Combat
+{
+    public class BodyDisposal : MonoBehaviour
+    {
+        [SerializeField] float timeToFadeOut = 10f;
+        [SerializeField] SkinnedMeshRenderer mesh = default;
+
+        Health health;
+        Material material;
+        float fadeOutTime = 0f;
+        bool fadeOut = false;
+
+        private void Awake()
+        {
+            health = GetComponent<Health>();
+            material = mesh.material;
+        }
+
+        private void Update()
+        {
+            if(health.IsDead())
+            {
+                fadeOutTime += Time.deltaTime;
+                if(fadeOutTime >= timeToFadeOut && !fadeOut)
+                {
+                    fadeOut = true;
+                    StartCoroutine(StartFadeOut());
+                }
+            }
+        }
+
+        private IEnumerator StartFadeOut()
+        {
+            while(material.color.a > 0.1f)
+            {
+                material.color = new Color(material.color.r, material.color.g, material.color.b, material.color.a - 0.01f);
+                yield return null;
+            }
+            Destroy(gameObject);
+        }
+    }
+}
