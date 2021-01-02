4a671716ab100e6085053ed11d712e473ed04a55
ArcticPass <riley_odlaw@hotmail.com>
2021-01-02 14:55:22 -0700

    Created Quest UI

---
+++ Assets/Scripts/UI/ShowHideUI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace RPG.UI
+{
+    public class ShowHideUI : MonoBehaviour
+    {
+        [SerializeField] KeyCode toggleKey = KeyCode.Escape;
+        [SerializeField] GameObject uiContainer = null;
+        [SerializeField] UnityEvent onToggle;
+
+        private void Start()
+        {
+            uiContainer.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if(Input.GetKeyDown(toggleKey))
+            {
+                Toggle();
+            }
+        }
+
+        public void Toggle()
+        {
+            uiContainer.SetActive(!uiContainer.activeSelf);
+            onToggle.Invoke();
+        }
+
+        public void SetToggle(bool toggle)
+        {
+            uiContainer.SetActive(toggle);
+        }
+    }
+}
