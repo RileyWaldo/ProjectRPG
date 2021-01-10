a77145f661bf045230e0130f9cb2c7327257a801
ArcticPass <riley_odlaw@hotmail.com>
2021-01-10 13:10:57 -0700

    Added HUD Controller and FollowCam script!

--- Assets/Scripts/Core/FollowCamera.cs
+++ Assets/Scripts/Core/FollowCamera.cs
@@ -1,14 +1,58 @@
 ﻿using UnityEngine;
+using Cinemachine;
 
 namespace RPG.Core
 {
     public class FollowCamera : MonoBehaviour
     {
-        [SerializeField] Transform target = default;
+        [Header("Camera Controls")]
+        [SerializeField] KeyCode turnRight = KeyCode.D;
+        [SerializeField] KeyCode turnLeft = KeyCode.A;
+        [SerializeField] KeyCode zoomIn = KeyCode.W;
+        [SerializeField] KeyCode zoomOut = KeyCode.S;
+        [SerializeField] float turnSensitivity = 180f;
+        [SerializeField] float zoomSensitivity = 8f;
+        [SerializeField] float minZoom = 5;
+        [SerializeField] float maxZoom = 15;
+
+        CinemachineVirtualCamera followCam;
+        CinemachineFramingTransposer framingTransposer;
+
+        private void Start()
+        {
+            followCam = GetComponent<CinemachineVirtualCamera>();
+            framingTransposer = followCam.GetCinemachineComponent<CinemachineFramingTransposer>();
+        }
 
         private void LateUpdate()
         {
-            transform.position = target.position;
+            ZoomCamera();
+            RotateCamera();
+        }
+
+        private void ZoomCamera()
+        {
+            float currentCameraDist = framingTransposer.m_CameraDistance;
+            if (Input.GetKey(zoomOut))
+                currentCameraDist += zoomSensitivity * Time.deltaTime;
+            if (Input.GetKey(zoomIn))
+                currentCameraDist -= zoomSensitivity * Time.deltaTime;
+
+            currentCameraDist = Mathf.Clamp(currentCameraDist, minZoom, maxZoom);
+
+            framingTransposer.m_CameraDistance = currentCameraDist;
+        }
+
+        private void RotateCamera()
+        {
+            float turnAngle = 0f;
+
+            if (Input.GetKey(turnRight))
+                turnAngle -= turnSensitivity * Time.deltaTime;
+            if (Input.GetKey(turnLeft))
+                turnAngle += turnSensitivity * Time.deltaTime;
+
+            transform.Rotate(new Vector3(0, turnAngle, 0), Space.World);
         }
     }
 }
---
+++ Assets/Scripts/UI/HUDController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using RPG.UI.Questing;
+
+namespace RPG.UI
+{
+    public class HUDController : MonoBehaviour
+    {
+        [Header("Windows")]
+        [SerializeField] GameObject inventoryWindow = null;
+        [SerializeField] GameObject questWindow = null;
+        [SerializeField] GameObject questInfoWindow = null;
+
+        [Header("Key Bindings")]
+        [SerializeField] KeyCode inventoryHotKey = KeyCode.I;
+        [SerializeField] KeyCode questHotKey = KeyCode.Q;
+
+        private void Start()
+        {
+            inventoryWindow.SetActive(false);
+            questInfoWindow.GetComponent<QuestInfoUI>().ShowVisible(false);
+            questWindow.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(inventoryHotKey))
+            {
+                ToggleInventory();
+            }
+
+            if (Input.GetKeyDown(questHotKey))
+            {
+                ToggleQuest();
+            }
+        }
+
+        public void ToggleInventory()
+        {
+            inventoryWindow.SetActive(!inventoryWindow.activeSelf);
+        }
+
+        public void ToggleQuest()
+        {
+            questWindow.SetActive(!questWindow.activeSelf);
+            questInfoWindow.GetComponent<QuestInfoUI>().ShowVisible(false);
+        }
+
+    }
+}
